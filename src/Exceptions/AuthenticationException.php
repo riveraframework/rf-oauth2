71ce8221d601944a4c3dd79f97850e554eb4b785
Pierre-Julien MAZENOT <pj.mazenot@gmail.com>
2018-03-18 13:01:48 -0400

    Add custom exceptions

---
+++ src/Exceptions/AuthenticationException.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file is part of the Rivera Framework OAuth2 package.
+ *
+ * (c) Pierre-Julien Mazenot <pj.mazenot@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Rf\OAuth2\Exceptions;
+
+/**
+ * Class AuthenticationException
+ *
+ * @package Rf\OAuth2\Exceptions
+ */
+class AuthenticationException extends \Exception {
+
+}
