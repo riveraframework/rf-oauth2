acf70af02aa69f03166cdb8bc58ce2889703bdd5
Pierre-Julien MAZENOT <pj.mazenot@gmail.com>
2018-03-18 13:05:33 -0400

    Add helpers class (WIP)

---
+++ src/OAuth2Helpers.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * This file is part of the Rivera Framework OAuth2 package.
+ *
+ * (c) Pierre-Julien Mazenot <pj.mazenot@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Rf\OAuth2 {
+
+    /**
+     * Class ApplicationHelpers
+     *
+     * @package Rf\Core\Application
+     */
+    class OAuth2Helpers {
+
+        /**
+         * This function goal is to use the Autoloader to load the current file containing
+         * useful helpers (functions)
+         */
+        public static function init() { }
+
+    }
+
+}
+
+namespace {
+
+	/**
+	 * Get the current user
+	 *
+	 * @return false
+	 */
+    function rf_current_user() {
+
+        // @TODO: Register hook
+
+    	return false;
+
+    }
+
+}
+
